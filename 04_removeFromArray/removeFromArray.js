a08030eaeed6be16c0a1c16b9c973f59410bc453
lecontroller <anujgupta@windowslive.com>
2022-05-05 00:42:29 +0700

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...Args) {
+    let args = Args;
+    for (let arg of args) {
+        if (arr.includes(arg)) {
+            const index = arr.indexOf(arg);
+            arr.splice(index, 1);
+        } 
+    }
+    return arr;
 };
 
 // Do not edit below this line
