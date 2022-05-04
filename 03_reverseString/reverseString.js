3fc43b810fe6aad2e9d8f61ae6739d3136854e2c
lecontroller <anujgupta@windowslive.com>
2022-05-04 23:58:11 +0700

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let characterArray = str.split('');
+    reversed = ''
+    for (i = characterArray.length-1; i >= 0; i--) {
+        reversed += characterArray[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
