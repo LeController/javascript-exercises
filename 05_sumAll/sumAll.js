5cb39b6701ecaa711afc27624148d8f6a730bad9
lecontroller <anujgupta@windowslive.com>
2022-05-05 10:19:09 +0700

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (typeof(startNum) !== 'number' || typeof(endNum) !== 'number' ) {
+        return "ERROR";
+    } else if (startNum < 0 || endNum < 0) {
+        return "ERROR"
+    }
 
+    if (startNum < endNum) {
+        var lowerNum = startNum;
+        var higherNum = endNum;
+    } else {
+        var lowerNum = endNum;
+        var higherNum = startNum;
+    }
+    let sum = 0;
+    for (let i = lowerNum; i <= higherNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
