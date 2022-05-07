ec945d57d659a0ecc4b4dec5031dd53d9973c03a
lecontroller <anujgupta@windowslive.com>
2022-05-08 00:32:08 +0700

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g ,"")
+    let characterArray = str.split('');
+    var j = str.length-1
+    for (i = 0; i < Math.floor(characterArray.length/2);i++) {
+        let start = characterArray[i];
+        let end = characterArray[j];
+        console.log(start, '  ', end)
+        if (start === end) {
+            j--
+            continue
+        }
+        else {return false}
+    }
+    return true
 };
 
 // Do not edit below this line
