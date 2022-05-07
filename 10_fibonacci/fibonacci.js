26b8df7b3121f51b1085f551c747045d400c786e
lecontroller <anujgupta@windowslive.com>
2022-05-08 00:41:16 +0700

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num
+    if (num < 0) {return "OOPS"}
+    let sequence = [1, 1]
+    for (i = 0; i < num; i++) {
+        const nextNum = sequence[i] + sequence[i+1]
+        sequence.push(nextNum)
+    }
+    return sequence[num-1]
 };
 
 // Do not edit below this line
