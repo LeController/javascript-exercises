3c766f849ff8d2d98238f6b1e17933c44de58f6f
lecontroller <anujgupta@windowslive.com>
2022-05-09 12:13:51 +0700

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    for (let person of obj) {
+      if (person.yearOfDeath) {
+        var age = person.yearOfDeath - person.yearOfBirth;
+      } else {
+        var age = new Date().getFullYear() - person.yearOfBirth;
+      }
+      person['age'] = age;
+    }
+    const ordered = obj.sort((a,b) => {
+      return a.age - b.age
+    })
+    return ordered[ordered.length-1]
 };
 
 // Do not edit below this line
